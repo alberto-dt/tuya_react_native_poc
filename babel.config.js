36a02c03606a99460530a58e87dc90790f021ac3
Luis Alberto De La Torre <lucho.mix34@gmail.com>
2025-06-20 06:10:08 -0500

    feat: React Native 0.73.6

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    '@babel/plugin-transform-private-methods',
-    '@babel/plugin-transform-class-properties',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
